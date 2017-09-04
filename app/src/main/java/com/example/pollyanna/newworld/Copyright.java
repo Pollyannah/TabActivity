61de129055dd9c8e046ffdb4395bdd715dcbb04c
Pollyannah <werucire52@gmail.com>
2017-09-04 14:32:44 -0400

    Initial comment

---
+++ app/src/main/java/com/example/pollyanna/newworld/Copyright.java
@@ -0,0 +1,13 @@
+package com.example.pollyanna.newworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Copyright extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_copyright);
+    }
+}
---
+++ app/src/main/java/com/example/pollyanna/newworld/TS.java
@@ -0,0 +1,13 @@
+package com.example.pollyanna.newworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ts);
+    }
+}
